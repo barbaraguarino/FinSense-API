4ca3adb91bec8e2cb5ad740f87e886a77296d2e3
Barbara <40583024+barbaraguarino@users.noreply.github.com>
2025-11-26 16:19:07 -0300

    Configurações Inicias

---
+++ src/main/java/com/finsense/api/FinSenseApiApplication.java
@@ -0,0 +1,13 @@
+package com.finsense.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinSenseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinSenseApiApplication.class, args);
+	}
+
+}
