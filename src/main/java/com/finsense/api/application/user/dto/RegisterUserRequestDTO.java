5c5f38ea660a621c82352475e8a6265b65309040
Barbara <40583024+barbaraguarino@users.noreply.github.com>
2025-12-05 14:29:08 -0300

    Adição das mensagens de erro para o email duplicado e mudança no DTO de requisição.

--- src/main/java/com/finsense/api/application/user/dto/RegisterUserRequestDTO.java
+++ src/main/java/com/finsense/api/application/user/dto/RegisterUserRequestDTO.java
@@ -1,4 +1,4 @@
-package com.finsense.api.application.dto;
+package com.finsense.api.application.user.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
